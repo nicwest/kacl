6a15afd464d0743a4dbd549b7aa78eeab9deb3c5
Nic West <nicwest.cb@gmail.com>
2017-07-23 15:44:25 +0100

    clean up

---
+++ README.md
@@ -0,0 +1,37 @@
+kacl
+====
+
+Command line utility for maintaining a changelog in the [Keep a
+Changelog](http://keepachangelog.com/en/1.0.0/) format
+
+
+Usage
+-----
+
+```
+$ kacl
+Command line utility for maintaining a changelog in the 
+Keep a Changelog format
+
+http://keepachangelog.com/en/1.0.0/
+
+Usage:
+  kacl [command]
+
+Available Commands:
+  added       Add a change to the list of Unreleased additions
+  changed     Add a change to the list of Unreleased changes
+  deprecated  Add a change to the list of Unreleased deprecations
+  fixed       Add a change to the list of Unreleased fixes
+  help        Help about any command
+  init        Initialize a CHANGELOG.md file
+  release     Create a new release
+  removed     Add a change to the list of Unreleased removals
+  security    Add a change to the list of Unreleased security updates
+
+Flags:
+      --config string   config file (default is $HOME/.kacl.yaml)
+  -h, --help            help for kacl
+
+Use "kacl [command] --help" for more information about a command.
+```
--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,3 @@
-// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
@@ -27,20 +14,13 @@ var cfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "kacl",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Command line utility for maintaining a changelog",
+	Long: `Command line utility for maintaining a changelog in the 
+Keep a Changelog format
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+http://keepachangelog.com/en/1.0.0/`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -48,40 +28,27 @@ func Execute() {
 	}
 }
 
-func init() { 
+func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kacl.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
-		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
 		home, err := homedir.Dir()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".kacl" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigName(".kacl")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
 
-	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
